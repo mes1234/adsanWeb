e7c792206c339780b16a55edfdcd266a926a1b7e
WK WK <witold.kepczynski@gmail.com>
2022-09-10 22:59:55 +0200

    added client

--- src/components/Welcome.tsx
+++ src/components/Welcome.tsx
@@ -30,8 +30,9 @@ class Welcome extends React.Component<IWelcome> {
           <Col md={6} >
             <Card className='card card-common h-100 border-0' >
               <Card.Header><h2><strong>O Mnie</strong></h2></Card.Header>
+              <img src="ania.jpg" className='card-image-common' ></img>
               <Card.Body>
-                <img src="https://random.imagecdn.app/500/550" className='card-image-common'></img>
+                
                 <Card.Text>
                   Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?
                 </Card.Text>
