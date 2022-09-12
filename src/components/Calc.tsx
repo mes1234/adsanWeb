c3c3706aba8ba130d580ab43bc5d18e5483448dd
WK WK <witold.kepczynski@gmail.com>
2022-09-12 23:20:48 +0200

    added address

--- src/components/Calc.tsx
+++ src/components/Calc.tsx
@@ -9,7 +9,8 @@ import { Configuration } from '../clients/function';
 
 
 
-const API = 'http://192.168.0.32:7071/api'
+// const API = 'http://192.168.0.32:7071/api'
+const API = 'https://developmentfunction.azurewebsites.net'
 
 interface IDataInputs {
   intValueIn: number,
