26973a959e665894cd22a7e4290159f55ebcb75c
WK WK <witold.kepczynski@gmail.com>
2022-09-13 23:27:09 +0200

    updates

--- src/components/Welcome.tsx
+++ src/components/Welcome.tsx
@@ -4,60 +4,32 @@ import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import { Button, Card, CardImg, ListGroup } from 'react-bootstrap';
-import Cookies from 'universal-cookie';
 import { faRuler } from "@fortawesome/free-solid-svg-icons";
 import { faCalculator } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 
-const API_GRPC = 'https://smartfirdge.azurewebsites.net'
 
 
 
 class Welcome extends React.Component<IWelcome> {
-  componentDidMount() {
-    try {
-      const cookies = new Cookies();
-      console.log(cookies.get('myCat')); // Pacman
-
-    }
-    catch (e) { }
-  }
   render() {
     return (
-      <Container  >
-        <Row>
-          <Col md={6} >
+      <div >
+        <Container  >
             <Card className='card card-common h-100 border-0' >
               <Card.Header><h2><strong>O Mnie</strong></h2></Card.Header>
-              <img src="dancing-banana.png" className='card-image-common' ></img>
+              <img src="ania.jpg" className='card-image-common' ></img>
               <Card.Body>
-                
                 <Card.Text>
-                  Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?
+                  <p>Projektantka instalacji sanitarnych z wieloletnim doświadczeniem przy projektowaniu obiektów użyteczności publicznej i mieszkaniowej.
+                    Biegła sądowa. Odpowiada za wykonywanie projektów z zakresu instalacji wodociągowo-kanalizacyjnych.</p>
+                  <p>Wdraża idee proekologiczne w budownictwie. Interesuję się zagospodarowaniem i wykorzystaniem wody deszczowej.</p>
                 </Card.Text>
               </Card.Body>
             </Card>
-          </Col>
-          <Col md={6} >
-            <Card className='card card-common h-100 border-0' >
-              <Card.Header><h2><strong>Oferta</strong></h2></Card.Header>
-              <Card.Body>
-                <Card.Text>
-                  <ListGroup >
-                    <ListGroup.Item><FontAwesomeIcon icon={faRuler} />  Cras justo odio</ListGroup.Item>
-                    <ListGroup.Item><FontAwesomeIcon icon={faCalculator} />  Dapibus ac facilisis in</ListGroup.Item>
-                    <ListGroup.Item><FontAwesomeIcon icon={faRuler} />  Morbi leo risus</ListGroup.Item>
-                    <ListGroup.Item><FontAwesomeIcon icon={faCalculator} />  Porta ac consectetur ac</ListGroup.Item>
-                    <ListGroup.Item><FontAwesomeIcon icon={faRuler} />  Vestibulum at eros</ListGroup.Item>
-                  </ListGroup>
-                </Card.Text>
-              </Card.Body>
-            </Card>
-          </Col>
-        </Row>
-      </Container>
-
+        </Container>
+      </div>
     );
   }
 }
